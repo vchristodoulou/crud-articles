7127c263e7c49cdec6f72a437490745bd9108adf
vchristodoulou <vchristodoulou1@gmail.com>
2020-11-04 12:16:36 +0200

    Subject: Added Article-Edit content functionality
    Changes:
    * Render Article
    * Added form to submit changes
    * Added content textarea to manipulate data

---
+++ client/src/components/Articles/edit/index.js
@@ -0,0 +1,111 @@
+import React, { Component } from "react";
+
+import ArticleService from "../../../services/article.service";
+
+
+export default class ArticleEdit extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            id: '',
+            title: '',
+            content: '',
+            description: '',
+            error: ''
+        };
+
+        this.handleChangeContent = this.handleChangeContent.bind(this);
+        this.handleEditSubmit = this.handleEditSubmit.bind(this);
+    }
+
+    handleChangeContent(event) {
+        this.setState({content: event.target.value});
+    }
+
+    handleEditSubmit(event) {
+        const { title } = this.props.match.params;
+        event.preventDefault();
+
+        ArticleService.edit(title, this.state.content)
+            .then(
+                response => {
+                    console.log(response);
+                    this.setState({
+                        id: response.data._id,
+                        title: response.data.title,
+                        content: response.data.content,
+                        description: response.data.description
+                    });
+                },
+                error => {
+                    this.setState({
+                        error:
+                            (error.response &&
+                                error.response.data &&
+                                error.response.data.message) ||
+                            error.message ||
+                            error.toString()
+                    });
+                }
+
+            )
+    }
+
+    componentDidMount() {
+        const { title } = this.props.match.params;
+        console.log(title);
+
+        ArticleService.getByTitleWithContent(title)
+            .then(
+                this.getArticleSuccess(),
+                this.getArticleError()
+            )
+    }
+
+    getArticleSuccess() {
+        return (
+            response => {
+                console.log(response);
+                this.setState({
+                    id: response.data[0]._id,
+                    title: response.data[0].title,
+                    content: response.data[0].content,
+                    description: response.data[0].description
+                });
+            }
+        )
+    }
+
+    getArticleError() {
+        return (
+            error => {
+                this.setState({
+                    error:
+                        (error.response &&
+                            error.response.data &&
+                            error.response.data.message) ||
+                        error.message ||
+                        error.toString()
+                });
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <h1>{this.state.title}</h1>
+                <small>{this.state.description}</small>
+                <hr/>
+                <form onSubmit={this.handleEditSubmit}>
+                    <div className="form-group">
+                        <textarea className="form-control" rows="10"
+                                  value={this.state.content} onChange={this.handleChangeContent} />
+                    </div>
+                    <button className="btn btn-primary" type="submit">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
