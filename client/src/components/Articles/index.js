be70a053b7c66d076da5626dd60a2fc6163c33dd
vchristodoulou <vchristodoulou1@gmail.com>
2020-11-04 12:01:34 +0200

    Subject: Added Article content/no content option
    Changes:
    * Radio content option to render Articles

--- client/src/components/Articles/index.js
+++ client/src/components/Articles/index.js
@@ -11,12 +11,56 @@ export default class Articles extends Component {
 
         this.state = {
             articles: [],
-            error: ""
+            error: ''
         };
+
+        this.onChangeContentRadio = this.onChangeContentRadio.bind(this);
     }
 
-    componentDidMount() {
-        ArticleService.get().then(
+    onChangeContentRadio(event) {
+        console.log(event.target.value);
+        if (event.target.value === 'noContent') {
+            this.getArticles();
+        } else if (event.target.value === 'withContent') {
+            this.getArticlesWithContent();
+        }
+    }
+
+    getArticlesSuccess() {
+        return (
+            response => {
+                this.setState({
+                    articles: response.data
+                });
+            }
+        )
+    }
+
+    getArticlesError() {
+        return (
+            error => {
+                this.setState({
+                    error:
+                        (error.response &&
+                            error.response.data &&
+                            error.response.data.message) ||
+                        error.message ||
+                        error.toString()
+                });
+            }
+        )
+    }
+
+    getArticles() {
+        ArticleService.get()
+            .then(
+                this.getArticlesSuccess(),
+                this.getArticlesError
+            );
+    }
+
+    getArticlesWithContent() {
+        ArticleService.getWithContent().then(
             response => {
                 this.setState({
                     articles: response.data
@@ -35,9 +79,17 @@ export default class Articles extends Component {
         );
     }
 
+    componentDidMount() {
+        this.getArticles();
+    }
+
     render() {
         return (
             <div className="container mt-3">
+                <div className="row mb-3" onChange={this.onChangeContentRadio}>
+                    <input className="mr-1" type="radio" value="noContent" name="contentRadios" /> No Content
+                    <input className="ml-3 mr-1" type="radio" value="withContent" name="contentRadios" /> With Content
+                </div>
                 <div className="row">
                     {Object.entries(this.state.articles).map(([key, value], i) => {
                         return (
