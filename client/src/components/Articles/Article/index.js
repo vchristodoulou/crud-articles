23b1adea89c7183dd2b3ca90572e1ff7934f9ef6
vchristodoulou <vchristodoulou1@gmail.com>
2020-11-05 13:44:59 +0200

    Subject: Added Category info to state
    Changes:
    * Saved category name and selected category to state
    * Render view based on this info

--- client/src/components/Articles/Article/index.js
+++ client/src/components/Articles/Article/index.js
@@ -15,7 +15,7 @@ export default class Article extends Component {
             ArticleService.deleteById(this.props.id)
                 .then(
                     response => {
-                        console.log(response);
+                        // console.log(response);
                         this.props.onDeleteArticle(this.props.id);
                     },
                     error => {
@@ -31,7 +31,6 @@ export default class Article extends Component {
             ArticleService.deleteByTitle(this.props.title)
                 .then(
                     response => {
-                        console.log(response);
                         this.props.onDeleteArticle(this.props.id);
                     },
                     error => {
@@ -43,55 +42,70 @@ export default class Article extends Component {
     }
 
     render() {
-        const  { id, title, content, description } = this.props;
+        const  { id, title, content, description, category_name, selectedCategoryName } = this.props;
 
         return (
             <div className="col-12">
-                <div className="card mb-4 box-shadow">
-                    <h5 className="card-header">{title}</h5>
-                    <div className="card-body">
-                        <small className="card-text">{description}</small>
-                        <hr/>
-                        <p>{content}</p>
-                        <div className="d-flex justify-content-between align-items-center">
-                            <div className="btn-group">
-                                <div className="dropdown">
-                                    <button className="tn btn-sm btn-outline-secondary dropdown-toggle" type="button"
-                                            id="dropdownView" data-toggle="dropdown" aria-haspopup="true"
-                                            aria-expanded="false">
-                                        View
-                                    </button>
-                                    <div className="dropdown-menu" aria-labelledby="dropdownView">
-                                        <Link className="dropdown-item"
-                                              to={`/articles/${id}`}
-                                        >No content</Link>
-                                        <Link className="dropdown-item"
-                                              to={`/articles/${id}/?content=true`}
-                                        >With content</Link>
+                { ((category_name === selectedCategoryName) || (selectedCategoryName === ''))  &&
+                    <div className="card mb-4 box-shadow">
+                        <div className="card-header">
+                            <h5>{title}</h5>
+                            <p className="text-right">{category_name}</p>
+                        </div>
+                        <div className="card-body">
+                            <small className="card-text">{description}</small>
+                            <hr/>
+                            <p>{content}</p>
+                            <div className="d-flex justify-content-between align-items-center">
+                                <div className="btn-group">
+                                    <div className="dropdown">
+                                        <button className="tn btn-sm btn-outline-secondary dropdown-toggle"
+                                                type="button"
+                                                id="dropdownView" data-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false">
+                                            View
+                                        </button>
+                                        <div className="dropdown-menu" aria-labelledby="dropdownView">
+                                            <Link className="dropdown-item"
+                                                  to={`/articles/${id}`}
+                                            >BY ID (no content)</Link>
+                                            <Link className="dropdown-item"
+                                                  to={`/articles/${id}/?content=true`}
+                                            >BY ID (with content)</Link>
+                                            <Link className="dropdown-item"
+                                                  to={`/articles/title/${title}`}
+                                            >BY TITLE (no content)</Link>
+                                            <Link className="dropdown-item"
+                                                  to={`/articles/title/${title}/?content=true`}
+                                            >BY TITLE (with content)</Link>
+                                        </div>
                                     </div>
-                                </div>
-                                <Link className="btn btn-sm btn-outline-secondary" type="button"
-                                      to={`/articles/edit/${title}`}
-                                >Edit
-                                </Link>
+                                    <Link className="btn btn-sm btn-outline-secondary" type="button"
+                                          to={`/articles/edit/${title}`}
+                                    >Edit
+                                    </Link>
 
-                                <div className="dropdown">
-                                    <button className="tn btn-sm btn-outline-secondary dropdown-toggle" type="button"
-                                            id="dropdownDelete" data-toggle="dropdown" aria-haspopup="true"
-                                            aria-expanded="false">
-                                        Delete
-                                    </button>
-                                    <div className="dropdown-menu" aria-labelledby="dropdownDelete">
-                                        <button className="dropdown-item" onClick={() => this.onDeleteById()}
-                                        >By Id</button>
-                                        <button className="dropdown-item" onClick={() => this.onDeleteByTitle()}
-                                        >By Title</button>
+                                    <div className="dropdown">
+                                        <button className="tn btn-sm btn-outline-secondary dropdown-toggle"
+                                                type="button"
+                                                id="dropdownDelete" data-toggle="dropdown" aria-haspopup="true"
+                                                aria-expanded="false">
+                                            Delete
+                                        </button>
+                                        <div className="dropdown-menu" aria-labelledby="dropdownDelete">
+                                            <button className="dropdown-item" onClick={() => this.onDeleteById()}
+                                            >By Id
+                                            </button>
+                                            <button className="dropdown-item" onClick={() => this.onDeleteByTitle()}
+                                            >By Title
+                                            </button>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                }
             </div>
         );
     }
